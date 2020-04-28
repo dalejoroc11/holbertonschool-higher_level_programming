f5b7e958cafdee5c1249559c0ebc00850ff0ea4d
dalejoroc11 <diegorojas279@hotmail.com.com>
2020-04-28 19:20:09 +0000

    higher

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for iterator in range(97, 123):
      if (iterator != 101 and iterator != 113):
-          print("{:c}".format(iterator), end="")
+        print('{:c}'.format(iterator), end='')
