bb0df97ee1ded05c2c112dc579fc7450e427fbd4
dalejoroc11 <diegorojas279@hotmail.com.com>
2020-04-28 19:28:54 +0000

    higher

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for iterator in range(0, 100):
     if iterator < 99:
-       print("{:02d}, ".format(iterator), end="")
+        print("{:02d}, ".format(iterator), end="")
     else:
-       print("{:02d}".format(iterator))
+        print("{:02d}".format(iterator))
