f6c121582f133e7c9fb482304226e7d959f8b0c9
dalejoroc11 <diegorojas279@hotmail.com.com>
2020-04-28 19:41:29 +0000

    higher

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-for iterator in range(0,10):
+for iterator in range(0,9):
     for iterator2 in range(iterator + 1, 10):
-        if(iterator != 8):
-            print("{}{}".format(iterator,iterator2), end=", ")
-        else:
-            print("{}{}".format(iterator, iterator2))
+        if(iterator == 8):
+            print("{:d}{:d}".format(iterator,iterator2))
+            break
+            print("{:d}{:d}".format(iterator, iterator2), end=", ")
