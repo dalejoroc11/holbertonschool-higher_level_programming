c0ac5f1e448c4037cfaa9dbf37a010284a90035b
dalejoroc11 <diegorojas11@hotmail.com>
2020-04-30 19:57:29 +0000

    high

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+
+
 if __name__ == '__main__':
-    import hidden_4 as hidden
-    name = dir(hidden)
-    for ite in range(len(name)):
-        if name[ite][:2] != '__':
-            print("{:s".format(name[ite]))
+    import hidden_4
+    names = dir(hidden_4)
+    for i in range(len(names)):
+        if names[i][:2] != '__':
+            print('{:s}'.format(names[i]))
