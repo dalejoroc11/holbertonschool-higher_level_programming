d457310fa60f02a650696c11bfea4000e383d3c6
dalejoroc11 <diegorojas279@hotmail.com>
2020-05-08 03:21:44 +0000

    fix

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,15 +1,35 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if roman_string is None or isinstance(roman_string, str) is False:
-        return None
-    n = 0
-    roman = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
-    for i in roman_string:
-        n += roman.get(i, 0)
-    subs = {'CM': -200, 'CD': -200, 'XC': -20, 'XL': -20, 'IX': -2, 'IV': -2}
-    chars = ""
-    for i in range(0, len(roman_string) - 1):
-        chars = roman_string[i] + roman_string[i + 1]
-        if chars in subs:
-            n += subs[chars]
-    return n
+    val = 0
+    if roman_string and type(roman_string) is str and roman_string.isalpha():
+        rs = roman_string.upper()
+        for i in range(len(rs)):
+            if rs[i] is 'I':
+                if i + 1 >= len(rs) or rs[i + 1] is 'I':
+                    val += 1
+                else:
+                    val -= 1
+            elif rs[i] is 'V':
+                val += 5
+            elif rs[i] is 'X':
+                if i + 1 >= len(rs) or rs[i + 1] in 'IVX':
+                    val += 10
+                else:
+                    val -= 10
+            elif rs[i] is 'L':
+                val += 50
+            elif rs[i] is 'C':
+                if i + 1 >= len(rs) or rs[i + 1] in 'IVXLC':
+                    val += 100
+                else:
+                    val -= 100
+            elif rs[i] is 'D':
+                val += 500
+            elif rs[i] is 'M':
+                if i + 1 >= len(rs) or rs[i + 1] in 'IVXLCD':
+                    val += 1000
+                else:
+                    val -= 1000
+            else:
+                return 0
+    return val
