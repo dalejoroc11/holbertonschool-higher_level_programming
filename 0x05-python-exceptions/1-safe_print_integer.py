3f568ca837fe7a254687d18afecb0c94f8c72203
dalejoroc11 <diegorojas279@hotmail.com>
2020-05-18 19:45:47 +0000

    exceptions

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,2 +1,13 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
+    counter = 0
+    for iterator in range(x):
+        try:
+            print('{:d}'.format(my_list[iterator]), end='')
+            counter += 1
+        except (TypeError, ValueError):
+            continue
+        except IndexError:
+            raise
+    print()
+    return counter
