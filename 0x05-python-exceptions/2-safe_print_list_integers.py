84ae7914f863f3c83562f7ca232d1ac3014791e2
dalejoroc11 <diegorojas279@hotmail.com>
2020-05-18 21:32:42 +0000

    exceptions

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,2 +1,11 @@
 #!/usr/bin/python3
-def safe_print_list_integers(my_list=[], x=0):
+def safe_print_list_integers(my_list=[], r=0):
+    printt = 0
+    for iterator in range(0, r):
+        try:
+            print("{:d}".format(my_list[iterator]), end="")
+            printt += 1
+        except (ValueError, TypeError):
+            continue
+    print()
+    return printt
