66d7447b242764bee4694e7a41c9678f8dee5a83
dalejoroc11 <diegorojas279@hotmail.com>
2020-05-19 22:55:36 +0000

    correction zero task

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
+"""Write a class Square that defines a square"""
+
+
 class Square:
-    # class square
+    """class square"""
     def __init__(self, size):
-        ''' initialize and reserve data
+        """initialize and reserve data
         # (my object, arg1)
         arg size integer size new square
-        '''
+        """
         self.__size = size
