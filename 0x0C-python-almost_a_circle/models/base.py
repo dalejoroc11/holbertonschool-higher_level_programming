5583cc22aff5c3c9e5153d294af1e00ddecc618b
dalejoroc11 <diegorojas279@hotmail.com>
2020-06-10 01:48:05 +0000

    fix task base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,122 +1,99 @@
 #!/usr/bin/python3
-"""
-base model class
-"""
+""" --- """
+
 import json
-import csv
+import os
 
 
 class Base:
-    """
-    Represent base model
-    """
-
+    """ --- """
     __nb_objects = 0
-    """
-    atribut: number of instantiated Bases
-    """
 
     def __init__(self, id=None):
-        """
-        init new Base - Argument: id(int)
-        """
-        if id is not None:
-            self.id = id
-        else:
+        if id is None:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+        else:
+            self.id = id
 
     @staticmethod
     def to_json_string(list_dictionaries):
-        """
-        Task 15: return Json serialization of list dictionaries
-        """
-        if list_dictionaries is None or list_dictionaries == []:
+        """ ..."""
+        if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         return json.dumps(list_dictionaries)
 
     @classmethod
     def save_to_file(cls, list_objs):
-        """
-        Task 16: write json serialization of list of objects to a file
-        """
-        filename = cls.__name__ + ".json"
-        with open(filename, "w") as jsonfile:
+        """ ..."""
+        filename = "" + cls.__name__ + ".json"
+        text = []
+        with open(filename, mode='w', encoding='UTF-8') as f:
             if list_objs is None:
-                jsonfile.write("[]")
+                return f.write(cls.to_json_string(None))
             else:
-                list_dicts = [o.to_dictionary() for o in list_objs]
-                jsonfile.write(Base.to_json_string(list_dicts))
+                for i in list_objs:
+                    text.append(i.to_dictionary())
+                json_list = cls.to_json_string(text)
+            return f.write(json_list)
 
     @staticmethod
     def from_json_string(json_string):
-        """
-        Task 17: return deseriatlization of json string
-        """
-        if json_string is None or json_string == []:
+        """ ..."""
+        if json_string is None or len(json_string) <= 0:
             return []
-        return json.loads(json_string)
+        return list(json.loads(json_string))
 
     @classmethod
     def create(cls, **dictionary):
-        """
-        Task 18: return class instance from a dictionary of attribute
-        """
-        if dictionary and dictionary != {}:
-            if cls.__name__ == "Rectangle":
-                new = cls(1, 1)
-            else:
-                new = cls(1)
-            new.update(**dictionary)
-            return new
+        """ ..."""
+        if cls.__name__ == "Rectangle":
+            aux = cls(1, 1)
+        elif cls.__name__ == "Square":
+            aux = cls(560)
+        aux.update(**dictionary)
+        return aux
 
     @classmethod
     def load_from_file(cls):
-        """
-        Task 19: return list classes instantiate from file of json string
-        """
-
-        filename = str(cls.__name__) + ".json"
-        try:
-            with open(filename, "r") as jsonfile:
-                list_dicts = Base.from_json_string(jsonfile.read())
-                return [cls.create(**d) for d in list_dicts]
-        except IOError:
+        """ ..."""
+        h = "" + cls.__name__ + ".json"
+        text = ""
+        newlist = []
+        if os.path.exists(h) is False:
             return []
+        with open(h, "r", encoding="UTF-8") as f:
+            text = f.read()
+            obj = cls.from_json_string(text)
+            for i in obj:
+                newlist.append(cls.create(**i))
+        return newlist
 
     @classmethod
     def save_to_file_csv(cls, list_objs):
-        """
-        Task 20: write the CSV serialization of list of objects to file
-        """
-        filename = cls.__name__ + ".csv"
-        with open(filename, "w", newline="") as csvfile:
-            if list_objs is None or list_objs == []:
-                csvfile.write("[]")
+        """ ..."""
+        filename = "" + cls.__name__ + ".csv"
+        ls = []
+        with open(filename, mode='w', encoding='UTF-8') as f:
+            if list_objs is None:
+                return f.write(cls.to_json_string(None))
             else:
-                if cls.__name__ == "Rectangle":
-                    fieldnames = ["id", "width", "height", "x", "y"]
-                else:
-                    fieldnames = ["id", "size", "x", "y"]
-                writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-                for obj in list_objs:
-                    writer.writerow(obj.to_dictionary())
+                for i in list_objs:
+                    ls.append(i.to_dictionary())
+                json_list = cls.to_json_string(ls)
+            return f.write(json_list)
 
     @classmethod
     def load_from_file_csv(cls):
-        """
-        Task 20: return list of classes instance from CSV file
-        """
-        filename = cls.__name__ + ".csv"
-        try:
-            with open(filename, "r", newline="") as csvfile:
-                if cls.__name__ == "Rectangle":
-                    fieldnames = ["id", "width", "height", "x", "y"]
-                else:
-                    fieldnames = ["id", "size", "x", "y"]
-                list_dicts = csv.DictReader(csvfile, fieldnames=fieldnames)
-                list_dicts = [dict([k, int(v)] for k, v in d.items())
-                              for d in list_dicts]
-                return [cls.create(**d) for d in list_dicts]
-        except IOError:
+        """ ..."""
+        h = "" + cls.__name__ + ".csv"
+        text = ""
+        newlist = []
+        if os.path.exists(h) is False:
             return []
+        with open(h, "r", encoding="UTF-8") as f:
+            text = f.read()
+            obj = cls.from_json_string(text)
+            for i in obj:
+                newlist.append(cls.create(**i))
+        return newlist
