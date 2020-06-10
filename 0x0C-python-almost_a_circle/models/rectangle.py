9590198b53bc6be4f3d728f4bfd946e90251fc36
dalejoroc11 <diegorojas279@hotmail.com>
2020-06-10 00:25:02 +0000

    fix task two

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -69,8 +69,7 @@ class Rectangle(Base):
         if value < 0:
             raise ValueError("x must be >= 0")
         self.__x = value
-
-            return
+        return
 
         [print("") for y in range(self.y)]
         for h in range(self.height):
@@ -99,14 +98,15 @@ class Rectangle(Base):
             for arg in args:
                 if a == 0:
                     if arg is None:
-                    self.width = v
-                    self.x = v
-                elif k == "y":
-        """
-        Task 13: return dictionary representation of Rectangle
-        """
-        return {"id": self.id, "width": self.width,
-                "height": self.height, "x": self.x, "y": self.y}self.y = v
+                        self.width = v
+                        self.x = v
+                    elif k == "y":
+                        """
+                        Task 13: return dictionary representation of Rectangle
+                        """
+                        return {"id": self.id, "width": self.width, "height":
+                                self.height, "x": self.x, "y":
+                                self.y}self.y = v
 
     def to_dictionary(self):
                 elif k == "height":
@@ -121,39 +121,44 @@ class Rectangle(Base):
                         self.id = arg
                 if k == "id":
                     if v is None:
-                elif a == 1:
-                    self.width = arg
-            for k, v in kwargs.items():
-                elif a == 2:
-                    self.height = arg
-        elif kwargs and len(kwargs) != 0:
-                elif a == 3:
-                a += 1
-
+                        elif a == 1:
+                            self.width = arg
+                    for k, v in kwargs.items():
+                        elif a == 2:
+                            self.height = arg
+                        elif kwargs and len(kwargs) != 0:
+                            elif a == 3:
+                                a += 1
                     self.x = arg
                 elif a == 4:
                     self.y = arg
+
+
 @property
-        if self.width == 0 or self.height == 0:
-            print("")
-        """
-        Task 5 and 7: Diplay print Rectangle instance with # & update x-y
-        """
-def y(self):
+if self.width == 0 or self.height == 0:
+    print("")
+    """
+    Task 5 and 7: Diplay print Rectangle instance with # & update x-y
+    """
+    def y(self):
         return self.__y
+
     def display(self):
 
         return self.width * self.height
 
         self.__y = value
 
-        """
+
 def area(self):
         """
         Task 4: Return area Rectangle
+        """
 raise TypeError("y must be an integer")
-        if value < 0:
-            raise ValueError("y must be >= 0")
-    @y.setter
-    def y(self, value):
-        if type(value) != int:
+if value < 0:
+    raise ValueError("y must be >= 0")
+
+
+@y.setter
+def y(self, value):
+    if type(value) != int:
