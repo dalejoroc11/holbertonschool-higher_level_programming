0ce52f533ce0b42eb1d8c57a07c8868b1f060ed9
dalejoroc11 <diegorojas279@hotmail.com>
2020-07-15 01:24:21 +0000

    fix four and seven task

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,7 +1,11 @@
 -- Write a script that creates the database hbtn_0d_usa
 -- and the table cities (in the database hbtn_0d_usa) on your MySQL server. 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
-       PRIMARY KEY(id),
-       id INT NOT NULL AUTO_INCREMENT,
-       name VARCHAR(256) NOT NULL);
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities(
+    id INT NOT NULL UNIQUE AUTO_INCREMENT,
+    state_id INT NOT NULL,
+    name VARCHAR(256) NOT NULL,
+    PRIMARY KEY (ID),
+    FOREIGN KEY (state_id) REFERENCES states(id)
+);
